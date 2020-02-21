396aa7cf98baadfb9253c42498e9efe2000e2b48
张旭 <zhangxu3486432@gmail.com>
2020-02-21 21:55:19 +0800

    更改数据库设计，区分开发和发布模式，修复 bug

--- deploy/init/cpd.sql
+++ deploy/init/cpd.sql
@@ -4,18 +4,28 @@ use news;
 
 create table if not exists cpd_news
 (
-	id varchar(40) not null,
-	url varchar(255) not null,
+	news_id varchar(50) not null primary key,
 	title varchar(255) not null,
-	content text not null,
 	category varchar(5) not null,
 	source varchar(50) not null,
 	date varchar(30) not null,
-	news_id varchar(50) not null,
-	page int not null,
+	page_total int not null,
+	entry_time datetime not null default CURRENT_TIMESTAMP  comment '入库时间',
 	constraint data_id_uindex
-		unique (id)
+        unique (news_id)
 );
 
-alter table data
-	add primary key (id);
+create table if not exists cpd_news_content
+(
+	news_id varchar(40) not null primary key,
+	request_id varchar(40) not null,
+	url varchar(255) not null,
+	content text not null,
+	page int not null,
+	constraint data_id_uindex
+        unique (request_id),
+	FOREIGN KEY fk_news(news_id)
+    REFERENCES cpd_news(news_id)
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT
+);
